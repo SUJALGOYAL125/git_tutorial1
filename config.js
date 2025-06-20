bcb6def00c24b5014c8df9389c387ecddf28013e
sujal goyal <digitalassandhsujal@gmail.com>
2025-06-20 19:54:10 +0530

    version 1

---
+++ config.js
@@ -0,0 +1,2 @@
+console.log('config.js');
+console.log('This file is used to configure the application.');
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log("Loading config.js");
+console.log("This file is used to configure the application.");
